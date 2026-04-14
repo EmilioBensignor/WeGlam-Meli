81f21df949a4a82aee7440f33a36923b358c55a4
Nicolas Rosso <nicolas.rossofor@gmail.com>
2026-04-13 21:42:14 -0300

    Revert "add sync"
    This reverts commit 2ec7e480368c5ea930426e232b8346dd0f3b2b4f.

--- app/layouts/dashboard.vue
+++ app/layouts/dashboard.vue
@@ -10,16 +10,6 @@
           </template>
 
           <template #right>
-            <UTooltip text="Sincronizar productos y métricas">
-              <UButton
-                color="neutral"
-                variant="ghost"
-                :icon="syncing ? 'i-tabler-loader-2' : 'i-tabler-refresh'"
-                :class="['text-on-surface', syncing && 'animate-spin']"
-                :disabled="syncing"
-                @click="runSync"
-              />
-            </UTooltip>
             <UColorModeButton color="neutral" variant="ghost" class="text-on-surface" />
           </template>
         </UDashboardNavbar>
@@ -31,7 +21,3 @@
     </UDashboardPanel>
   </UDashboardGroup>
 </template>
-
-<script setup>
-const { syncing, runSync } = useSync()
-</script>
